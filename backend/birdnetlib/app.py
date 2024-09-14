2f6336ff630f00719acb855d4dcc1fa0bb7cb228
ppranav03 <ppranav03@vt.edu>
2024-09-14 00:35:57 -0400

    more changes

--- backend/birdnetlib/app.py
+++ backend/birdnetlib/app.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+blob = ""
 coordinates = {}
 uri = "mongodb+srv://admin:admin@cluster0.xswxz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(uri, server_api=ServerApi('1'))
@@ -14,10 +15,20 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 def index():
     return jsonify(message = "Welcome to the Aviate API")
 
+@app.route('/api/getblob', methods = ["POST"])
+def retrieve_blob():
+    global blob
+    data = request.json
+    try:
+        blob = data.get('blob')
+        assert blob is not None
+    except:
+        raise ValueError("Error: Blob does not exist")
+
+#localhost:5000
 @app.route('/api/analyze', methods=['GET'])
 def analyze_sounds():
     global client
-
     try:
         client.admin.command('ping')
         print("Pinged your deployment. You successfully connected to MongoDB!")
@@ -34,18 +45,18 @@ def analyze_sounds():
     return jsonify(message="Analysis complete", data=data)  
 
 
-# @app.route('/api/coordinates', methods=['POST'])
-# def receive_coordinates():
-#     global coordinates
-#     data = request.json
-#     try:
-#         latitude = data.get('latitude')
-#         longitude = data.get('longitude')
+@app.route('/api/coordinates', methods=['POST'])
+def receive_coordinates():
+    global coordinates
+    data = request.json
+    try:
+        latitude = data.get('latitude')
+        longitude = data.get('longitude')
 
-#         assert latitude is not None and longitude is not None
+        assert latitude is not None and longitude is not None
     
-#     except:
-#         raise ValueError("Error: Latitude or Longitude does not exist")
+    except:
+        raise ValueError("Error: Latitude or Longitude does not exist")
 
-#     coordinates = {"latitude" : latitude, "longitude": longitude}
-#     return jsonify(message = "Coordinates received and stored")
+    coordinates = {"latitude" : latitude, "longitude": longitude}
+    return jsonify(message = "Coordinates received and stored")
