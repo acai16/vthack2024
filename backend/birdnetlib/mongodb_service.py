9361ae53b1d7b985e9d44ea1a0ea9f68ead4e99a
JusticeJJackson <alan.cai.work@gmail.com>
2024-09-14 16:57:26 -0400

    updated mongodb serveice

--- backend/birdnetlib/mongodb_service.py
+++ backend/birdnetlib/mongodb_service.py
@@ -14,13 +14,21 @@ class MongodbService:
     # def __del__(self):
     #     self.client.close()
 
-    def upload_file(self, file_path):
+    def upload_file(self, file_path, bird_dict, hike_dict):
+        bird_info_collection = self.db["bird_info"]
+        hiker_info_collection = self.db["hike_info"]
         with open(file_path, "rb") as file:
             # Upload the file to GridFS
             file_id = self.bucket.upload_from_stream(
                 file_path, file, metadata={"contentType": "audio/mpeg"}
             )
+        bird_dict["file_id"] = file_id
+        hike_dict["user_id"] = hi
+        bird_info_obj_id = bird_info_collection.insert_one(bird_dict)
+
         print(f"Uploaded file with ID: {file_id}")
+        print(f"Bird Object ID: {bird_info_obj_id}")
+        print("")
         return file_id
 
     def download_file(self, file_id):
